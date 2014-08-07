d862251a00ae2835aab19fd8afcd4349badc86f8
Eduard Mombiela <mombiela.eduard@gmail.com>
2014-08-07 18:55:34 +0200

    First commit

---
+++ src/Emombiela/Iban/IbanServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Emombiela\Iban;
+
+use Illuminate\Support\ServiceProvider;
+
+class IbanServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
